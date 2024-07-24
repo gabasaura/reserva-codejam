6c41d171878d4ef7f4f279830e140642aaccda26
Nicolas <n3kguzman@gnmail.com>
2024-07-24 17:08:08 -0400

    ultmo commit

--- back/models.py
+++ back/models.py
@@ -5,7 +5,7 @@ db = SQLAlchemy()
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(80), unique=True, nullable=False)
+    name = db.Column(db.String(80), unique=False, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(200), nullable=False)  
     reservations = db.relationship('Reservation', backref='user')
