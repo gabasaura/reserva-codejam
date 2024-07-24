6518569c31b8d9b8354c694a280e3274ed2dcc11
gabasaura <gabriela.garin@gmail.com>
2024-07-24 16:21:29 -0400

    front

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -6,7 +6,6 @@ import Login from "./pages/Login";
 import injectContext from "./store/AppContext";
 import { ToastContainer } from 'react-toastify';
 import Navbar from "./components/Navbar";
-import UserProfile from "./pages/User";
 
 const App = () => {
 
@@ -17,7 +16,6 @@ const App = () => {
                     <Route path="/" element={<Home />} />
                     <Route path="/register" element={<Register />} />
                     <Route path="/login" element={<Login />} />
-                    <Route path="/user" element={<UserProfile />} />
                     <Route path="*" element={<NotFound />} />
                 </Routes>
                 <ToastContainer />
